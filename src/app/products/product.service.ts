292b07b48049089a9a7a2cd6ee96e21a89a014cd
Sen2pi <dr.karim.patatas@gmail.com>
2022-09-28 00:13:56 +0100

    add categ
    ada

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import  { from , Observable } from 'rxjs';
+import { Category } from '../site-layout/category';
 import { Product } from './product';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,33 +13,38 @@ export class ProductService {
   constructor(private httpClient : HttpClient) { }
  
   createProduct(productBody: any):Observable<Product>{
-   const baseUrl="http://localhost:4000/product";
+   const baseUrl="http://localhost:4200/product";
    return this.httpClient.post<Product>(baseUrl, productBody)
   }
 
   viewProduct(productId: string):Observable<Product>{
-   const baseUrl="http://localhost:4000/product/"+productId;
+   const baseUrl="http://localhost:4200//product/"+productId;
    return this.httpClient.get<Product>(baseUrl)
   }
 
   updateProduct(productId: string, productBody: any):Observable<Product>{
-   const baseUrl="http://localhost:4000/product/"+productId;
+   const baseUrl="http://localhost:4200/product/"+productId;
    return this.httpClient.put<Product>(baseUrl, productBody)
   }
 
   deleteProduct(productId: string):Observable<Product>{
-   const baseUrl="http://localhost:4000/product/"+productId;
+   const baseUrl="http://localhost:4200/product/"+productId;
    return this.httpClient.delete<Product>(baseUrl)
   }
 
   searchCategoryProduct(categoryId: string):Observable<Product>{
-   const baseUrl="http://localhost:4000/product/category"+categoryId;
+   const baseUrl="http://localhost:4200/product/category"+categoryId;
    return this.httpClient.get<Product>(baseUrl)
   }
 
   searchDateProduct(dateParam: string):Observable<Product>{
-   const baseUrl="http://localhost:4000/product/date"+dateParam;
+   const baseUrl="http://localhost:4200/product/date"+dateParam;
    return this.httpClient.get<Product>(baseUrl)
+  }
+
+  getCategory(){
+    const categoryUrl="http://localhost:4200/categories";
+   return this.httpClient.get<Category>(categoryUrl)
   }
 
 }
